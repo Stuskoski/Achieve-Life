ed23a20f558264899026d0707da79a4813cb7822
mmurata3 <murata_m33@yahoo.com>
2015-11-17 16:03:02 -0600

    Working on adding in db, need to set a specific way to do this for all other things in website!

--- achieve/achieve/WebContent/controllers/SignupController.class.php
+++ achieve/achieve/WebContent/controllers/SignupController.class.php
@@ -15,24 +15,16 @@ class SignupController {
   		$returnId = 0;
   		try {
   			$db = Database::getDB();
-  			$query = "INSERT INTO users (firstName, lastName, email, phoneN, username, userPasswordHash, personalUrl, month, selectId, radioId)
-			            VALUES(:firstName, :lastName, :email, :phoneN, :username, :userPasswordHash, :personalUrl, :month, :selectId, :radioId)";
+  			$query = "INSERT INTO users (firstName, lastName, email, username, userPasswordHash)
+			            VALUES(:firstName, :lastName, :email, :phoneN, :username, :userPasswordHash)";
   			$statement = $db->prepare($query);
   			$statement->bindValue(":firstName", $firstName);
   			$statement->bindValue(":lastName", $lastName);
   			$statement->bindValue(":email", $email);
-  			$statement->bindValue(":phoneN", $phoneN);
   			$statement->bindValue(":username", $username);
   			$statement->bindValue(":userPasswordHash", $passHash);
-  			$statement->bindValue(":personalUrl", $personalUrl);
-  			$statement->bindValue(":month", $month);
-  			$statement->bindValue(":selectId", $selectId);
-  			$statement->bindValue(":radioId", $radioId);
   			$statement->execute();
   			$statement->closeCursor();
-  			$returnId = $db->lastInsertId("userId");
-  			$myTags = $myUser->getGenreList();
-  			NewUserDB::writeUserTags($db, $returnId, $myTags);
   		} catch ( PDOException $e ) { // Not permanent error handling
   			echo "<p>Error adding users ".$e->getMessage()."</p>";
   		}
--- achieve/achieve/WebContent/views/SignupView.class.php
+++ achieve/achieve/WebContent/views/SignupView.class.php
@@ -1,6 +1,5 @@
 <?php  
 class SignupView {
-//firstName, lastName, userName, email, password, gender	
   public static function show() {  	
 ?>
 <link href="assets/css/signup.css" rel="stylesheet">
@@ -8,64 +7,53 @@ class SignupView {
    <div class="overlay">
 	<div class="container">
 		<div class="row">
-			<form class="form-horizontal" role="form" action="" method="post">
+			<form class="form-horizontal" role="form" action="controllers/SignupController.class.php" method="post">
 				<div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2">
 					<h2>Signup Form</h2>
 					<div class="form-group">
 						<div class="col-xs-6" style="padding-right: 7px;">
-							<input type="text" class="input-small form-control" name="fname"
-								placeholder="First name"> <span class="form-control-feedback"
-								id="fnameErr-feedback" style="right: 7px;"></span> <span
-								class="help-block" id="fnameErr"></span>
+							<input type="text" class="input-small form-control" name="fname" placeholder="First name"> 
+							<span class="form-control-feedback" id="fnameErr-feedback" style="right: 7px;"></span> 
+							<span class="help-block" id="fnameErr"></span>
 						</div>
 						<div class="col-xs-6" style="padding-left: 7px;">
-							<input type="text" class="form-control" name="lname"
-								placeholder="Last name"> <span class="form-control-feedback"
-								id="lnameErr-feedback"></span> <span class="help-block"
-								id="lnameErr"></span>
-						</div>
-					</div>
-					<label class="radio-inline"><input type="radio" name="optM">Male</label>
-					<label class="radio-inline"><input type="radio" name="optF">Female</label>
-					<div class="form-group required">
-						<div class="col-xs-12">
-							<input type="email" class="form-control" name="email" required> <span
-								class="form-control-feedback" id="emailErr-feedback"></span> <span
-								class="help-block" id="emailErr"></span>
+							<input type="text" class="form-control" name="lname" placeholder="Last name">
+							<span class="form-control-feedback" id="lnameErr-feedback"></span> 
+							<span class="help-block" id="lnameErr"></span>
 						</div>
 					</div>
 					<div class="form-group required">
 						<div class="col-xs-12">
-							<input type="text" class="form-control" name="username" required> <span
-								class="form-control-feedback" id="usernameErr-feedback"></span> <span
-								class="help-block" id="usernameErr"></span>
+							<input type="email" class="form-control" name="email" placeholder="Email" required> 
+							<span class="form-control-feedback" id="emailErr-feedback"></span> 
+							<span class="help-block" id="emailErr"></span>
 						</div>
 					</div>
 					<div class="form-group required">
 						<div class="col-xs-12">
-							<input type="text" class="form-control" name="username" required> <span
-								class="form-control-feedback" id="usernameErr-feedback"></span> <span
-								class="help-block" id="usernameErr"></span>
+							<input type="text" class="form-control" name="username" placeholder="Username" required> 
+							<span class="form-control-feedback" id="usernameErr-feedback"></span> 
+							<span class="help-block" id="usernameErr"></span>
 						</div>
 					</div>
 					<div class="form-group required">
 						<div class="col-xs-12">
-							<input type="password" class="form-control" name="passwd" required> <span
-								class="form-control-feedback" id="passwdErr-feedback"></span> <span
-								class="help-block" id="passwdErr"></span>
+							<input type="password" class="form-control" name="passwd" placeholder="Password" required> 
+							<span class="form-control-feedback" id="passwdErr-feedback"></span> 
+							<span class="help-block" id="passwdErr"></span>
 						</div>
 					</div>
 					<div class="form-group required">
 						<div class="col-xs-12">
-							<input type="password" class="form-control" name="repasswd" required> <span
-								class="form-control-feedback" id="repasswdErr-feedback"></span> <span
-								class="help-block" id="repasswdErr"></span>
+							<input type="password" class="form-control" name="repasswd" placeholder="Retype password" required> 
+							<span class="form-control-feedback" id="repasswdErr-feedback"></span> 
+							<span class="help-block" id="repasswdErr"></span>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-12">
 					<div id="submitBtn">
-						<button type="submit" class="btn btn-white-lg">Submit</button>
+						<button type="submit" class="btn btn-black-lg">Submit</button>
 					</div>
 				</div>
 			</form>
