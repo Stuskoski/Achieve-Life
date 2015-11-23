d59cf38caf26a0a3d5f9a77146e3e0c0a603a9e7
mmurata3 <murata_m33@yahoo.com>
2015-11-22 23:57:47 -0600

    Adding more visual adjustments

--- achieve/achieve/WebContent/views/DashboardView.class.php
+++ achieve/achieve/WebContent/views/DashboardView.class.php
@@ -20,21 +20,18 @@ class DashboardView {
   						<div class="infoHead"><span class="h1">Welcome <?php echo $_SESSION['user_session']?></span></div>
   						<hr>
   					</div>
-  					<div class="col-md-4">
+  					<div class="col-md-2 infoSec br">
   						<div class="infoHead"><span class="h2">Stats</span></div>
 					  	<div class="info"><span>Title: <?php GetProfileInfo::getTitle($_SESSION['user_session']);?></span></div>
 					  	<div class="info"><span>Points: <?php GetProfileInfo::getPoints($_SESSION['user_session']);?></span></div>
 					  	<div class="info"><span>Rank: <?php GetProfileInfo::getRank($_SESSION['user_session']);?></span></div>
 					  	<div class="info"><span>Honesty Rating: <?php GetProfileInfo::getHonesty($_SESSION['user_session']);?>%</span></div>
   					</div>
-  					<div class="col-md-4">
-  					
+  					<div class="col-md-8 infoSec bl br">
+  						<div class="infoHead"><span class="h2">Dashboard</span></div>
   					</div>
-  					<div class="col-md-4">
-  					
-  					</div>
-  					<div class="col-xs-12" style="text-align: center; padding-top: 200px;">
-  						<hr>
+  					<div class="col-md-2 infoSec bl">
+  						<div class="infoHead"><span class="h2">Links</span></div>
   						<div class="link"><a href="friends">Friends List</a></div>
 					  	<div class="link"><a href="viewchallenges">View Challenges Sent To You</a></div>
 					  	<div class="link"><a href="verifychallenges">Verify Challenges You Sent</a></div>
